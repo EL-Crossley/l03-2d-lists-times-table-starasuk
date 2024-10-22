2862013eeeef7c1e0fdf7a5708a46cd8734c295d
starasuk <sashatarasuk10@gmail.com>
2024-10-22 12:59:46 -0400

    Update main.py
    it worked on pycharm ¯\_(ツ)_/¯

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-# Put your function here
+def timesTable(num):
+    list = []
+    for i in range(1, num+1):
+        # print(i)
+        list.append([])
+        # print(list)
+        for j in range(1, num+1):
+            temp = i*j
+            (list[i-1]).append(temp)
+
+    return list
 
 
-# testing
+
 nums = int(input())
 print(timesTable(nums))
